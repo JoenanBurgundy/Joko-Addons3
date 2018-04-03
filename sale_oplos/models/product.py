5126b9c055edf39305cd828b02593304fe622b67
Joenan Nadhir Rayhan <joenan@Joenans-MacBook.local>
2018-04-03 23:29:36 +0700

    sale_oplos update code

--- sale_oplos/models/product.py
+++ sale_oplos/models/product.py
@@ -19,9 +19,29 @@ class ProductTemplate(models.Model):
 #     oplos_desc = fields.Char(string='Oplos Description')
 #     oplos_sale_price = fields.Float(string='Oplos Sale Price', digits=dp.get_precision('Product Price'))
 
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+    
+    @api.multi
+    def name_get(self):
+        if self._context.get('code_only'):
+            result = []
+            for product in self:
+                result.append((product.id, "%s" % (product.default_code or product.name)))
+            return result
+        else:
+            return super(ProductProduct, self).name_get()
+
 class ProductOplos(models.Model):
     _name = 'product.oplos'
 
     product_id = fields.Many2one('product.product', string='Product')
     oplos_product_id = fields.Many2one('product.product', string='Oplos Code', required=True)
     oplos_desc = fields.Char(string='Oplos Description', required=True)
+    
+    @api.onchange('oplos_product_id')
+    def _onchange_oplos_product_id(self):
+        self.oplos_desc = self.oplos_product_id.name if self.oplos_product_id else False
+        return
+        
